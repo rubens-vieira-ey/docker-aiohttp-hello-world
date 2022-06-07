6750a5bd47cd13465fd32aba0e103aaa75678c61
Rubens <megapixmode@gmail.com>
2022-06-07 11:01:14 -0300

    1

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 from aiohttp import web
 
-
-async def handle(request):
+async def handle(request):      
     name = request.match_info.get('name', "World!")
     text = "Hello, " + name
     print('received request, replying with "{}".'.format(text))
     return web.Response(text=text)
-
 
 app = web.Application()
 app.router.add_get('/', handle)
